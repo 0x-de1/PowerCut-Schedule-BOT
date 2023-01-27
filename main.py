cfac0772ca5eca3c2d59e6184507da0ee05e7cc7
0x-de1 <dewanscode@gmail.com>
2023-01-27 11:45:43 +0530

    modified code to wait for a connection

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from cs50 import SQL
 import requests
 import os
 import logging
+from urllib import request, error
+from time import sleep
 
 
 def start_webdriver():
@@ -91,6 +93,7 @@ def get_outage_data(accounts):
                 f"account: {account['ceb_account']} , date: {date} schedule: No data"
             )
             continue
+        sleep(5)
 
     driver.quit()
     return date, accounts
@@ -155,7 +158,27 @@ def send_messege(messege_object):
     print(result)
 
 
+def wait_for_internet_connection():
+    while True:
+        try:
+            request.urlopen("http://www.google.com", timeout=2)
+            print("connected")
+            return
+        except error.URLError:
+            print("waiting for connection...")
+            sleep(20)
+
+
 def main():
+    # check API keys
+
+    if not os.environ.get("whatsapp_access_token") or not os.environ.get(
+        "whatsapp_phone_id"
+    ):
+        raise RuntimeError("API_KEY not set")
+
+    # Wait for internet connection
+    wait_for_internet_connection()
     # Configure CS50 Library to use SQLite database
     db = SQL("sqlite:///powercut_bot.db")
     # Select all unique accounts from db
